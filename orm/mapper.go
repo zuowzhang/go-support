a33ee9b98f4048caf5ae8d66a8b09c7419d774e0
张斌 <zbin@meizu.com>
2017-09-15 18:02:45 +0800

    添加部分代码

--- orm/mapper.go
+++ orm/mapper.go
@@ -6,5 +6,21 @@ type Mapper interface {
 }
 
 type MemoryMapper struct {
+	column2Field map[string]string
+	field2Column map[string]string
+}
 
+func NewMemoryMapper() Mapper {
+	return &MemoryMapper{
+		column2Field:make(map[string]string),
+		field2Column:make(map[string]string),
+	}
+}
+
+func (m *MemoryMapper)ColumnName(fieldName string) string {
+	return m.field2Column[fieldName]
+}
+
+func (m *MemoryMapper)FieldName(columnName string) string {
+	return m.column2Field[columnName]
 }
--- orm/table.go
+++ orm/table.go
@@ -1,6 +1,27 @@
 package orm
 
+import "reflect"
+
+const TAG_ORM string = "orm"
+
 type Table struct {
-	tableName string
-	mapper    Mapper
+	name  string
+	structType reflect.Type
+	mapper     Mapper
+}
+
+func NewTable(name string, modelBean interface{}) *Table {
+	t := reflect.TypeOf(modelBean)
+	mapper := NewMemoryMapper()
+	for i := 0; i < t.NumField(); i++ {
+		tag := t.Field(i).Tag.Get(TAG_ORM)
+		if tag != "" {
+
+		}
+	}
+	return &Table{
+		name:name,
+		structType:t,
+		mapper:mapper,
+	}
 }
